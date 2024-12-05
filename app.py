99f07598a7e4137d0a06fe1e82c7415de2422130
SSDshubhamdev <shubhamdev098@gmail.com>
2024-12-05 20:50:21 +0530

    original

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 def add(a, b):
+
     return a + b
 
+
 def subtract(a, b):
+
     return a - b
 
+
 if __name__ == "__main__":
+
     print("Addition of 2 and 3 is:", add(2, 3))
     print("Subtraction of 5 from 10 is:", subtract(10, 5))
--- pr.py
+++ pr.py
@@ -1,2 +1,10 @@
 print("Hello shubham")
+
+# Define the variables a and b
+a = 5
+b = 10
+
+# Now you can safely perform the addition
 c = a + b
+
+print("The result of a + b is:", c)

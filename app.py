ce51b937cce19dd56c615befe2312b7771646a12
SSDshubhamdev <94627726+SSDshubhamdev@users.noreply.github.com>
2024-12-05 21:12:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 def add(a, b):
-##app testing
+    # app testing
     return a + b
 
 
 def subtract(a, b):
-
     return a - b
 
 
 if __name__ == "__main__":
-
     print("Addition of 2 and 3 is:", add(2, 3))
     print("Subtraction of 5 from 10 is:", subtract(10, 5))
